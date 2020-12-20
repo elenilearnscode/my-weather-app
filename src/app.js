4c12999ce398b5ff2f2bd41d3c3d2c7a273e08ee
elenilearnscode <contact.enicolaides@gmail.com>
2020-12-20 15:50:43 +0000

    Updated the api url
    Updated the api url and set the let city as Maidstone.

--- src/app.js
+++ src/app.js
@@ -58,7 +58,8 @@ dateElement.innerHTML = formatDate(response.data.dt * 1000)
 
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Maidstone&&appid=${apiKey}&units=metric`;
+let city = "Maidstone";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 
 // let currentDate = new Date();
