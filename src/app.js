f171bd7a33a2826f77340e3bc5d4b0baf8eb2a80
elenilearnscode <contact.enicolaides@gmail.com>
2020-12-20 14:46:33 +0000

    Showing the current temperature
    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,85 +1,97 @@
-// DATE & TIME
+// // DATE & TIME
 
-function formatDate() {
-  let dayIndex = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = dayIndex[currentDate.getDay()];
+// function formatDate() {
+//   let dayIndex = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday"
+//   ];
+//   let day = dayIndex[currentDate.getDay()];
 
-  let hours = currentDate.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
+//   let hours = currentDate.getHours();
+//   if (hours < 10) {
+//     hours = `0${hours}`;
+//   }
 
-  let minutes = currentDate.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  return `${day}, ${hours}:${minutes}`;
+//   let minutes = currentDate.getMinutes();
+//   if (minutes < 10) {
+//     minutes = `0${minutes}`;
+//   }
+//   return `${day}, ${hours}:${minutes}`;
+// }
+//scattered clouds
+function displayTemperature(response) {
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let city = "Maidstone";
-var apiUrl = "";
-let unit = "metric"; // metric for celsius
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Maidstone&&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
 
-let currentDate = new Date();
-let date = document.querySelector("#date");
-date.innerHTML = formatDate(currentDate);
+// let currentDate = new Date();
+// let date = document.querySelector("#date");
+// date.innerHTML = formatDate(currentDate);
 
-// SEARCH ENGINE
+// // SEARCH ENGINE
 
-let celsiusTemperature = 12
+// let celsiusTemperature = 12
 
-  function showTemperature(response) {
-  celsiusTemperature = response.data.main.temp;
-  let cityName = document.querySelector("#city");
-  cityName.innerHTML = response.data.name;
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
+//   function showTemperature(response) {
+//   celsiusTemperature = response.data.main.temp;
+//   let cityName = document.querySelector("#city");
+//   cityName.innerHTML = response.data.name;
+//   let temperatureElement = document.querySelector("#temperature");
+//   temperatureElement.innerHTML = Math.round(celsiusTemperature);
+// }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  let citySearch = document.querySelector("#city-input");
-  let cityName = document.querySelector("#city");
-  cityName.innerHTML = citySearch.value;
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
-  searchCity(citySearch.value);
-}
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   let citySearch = document.querySelector("#city-input");
+//   let cityName = document.querySelector("#city");
+//   cityName.innerHTML = citySearch.value;
+//   let descriptionElement = document.querySelector("#description");
+//   let humidityElement = document.querySelector("#humidity");
+//   let windElement = document.querySelector("#wind");
+//   let dateElement = document.querySelector("#date");
+//   let iconElement = document.querySelector("#icon");
+//   searchCity(citySearch.value);
+// }
 
-function searchCity(city) {
-  apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`
-  }
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
+// function searchCity(city) {
+//   apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Maidstone&&appid=${apiKey}`;
+//   }
+// let searchForm = document.querySelector("#search-form");
+// searchForm.addEventListener("submit", handleSubmit);
 
-// CURRENT-CITY BUTTON
+// // CURRENT-CITY BUTTON
 
-function getPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let unit = "metric"; // metric for celsius
-  apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`
-  axios.get(apiUrl).then(showTemperature);
-}
+// function getPosition(position) {
+//   let lat = position.coords.latitude;
+//   let lon = position.coords.longitude;
+//   let unit = "metric"; // metric for celsius
+//   apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`
+//   axios.get(apiUrl).then(showTemperature);
+// }
 
 
-function displayFahrenheit(event){
-event.preventDefault();
-let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
-fahrenheitLink.classList.add("active");
-celsiusLink.classList.remove("active");
+// function displayFahrenheit(event){
+// event.preventDefault();
+// let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// fahrenheitLink.classList.add("active");
+// celsiusLink.classList.remove("active");
 // let temperatureElement = document.querySelector("#temperature");
 // temperatureElement.innerHTML = Math.round(fahrenheitTemperature)
 // }
@@ -101,18 +113,18 @@ celsiusLink.classList.remove("active");
 // temperatureElement.innerHTML = Math.round(celsiusTemperature)
 // }
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheit);
+// let fahrenheitLink = document.querySelector("#fahrenheit-link");
+// fahrenheitLink.addEventListener("click", displayFahrenheit);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsius);
+// let celsiusLink = document.querySelector("#celsius-link");
+// celsiusLink.addEventListener("click", displayCelsius);
 
-function startGeolocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(getPosition);
-}
+// function startGeolocation(event) {
+//   event.preventDefault();
+//   navigator.geolocation.getCurrentPosition(getPosition);
+// }
 
-let position = document.querySelector("#current-city-btn");
-position.addEventListener("click", startGeolocation);
+// let position = document.querySelector("#current-city-btn");
+// position.addEventListener("click", startGeolocation);
 
-search("Maidstone");
+// search("Maidstone");
