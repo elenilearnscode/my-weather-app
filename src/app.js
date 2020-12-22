f78e6046b6242c4262b92d067b094d8360346b02
elenilearnscode <contact.enicolaides@gmail.com>
2020-12-22 14:15:22 +0000

    Updated search feature and attempted to fix current location feature and Fahrenheit conversion.
    Removed all the unecessary comments which were confusing me. Made the search button work. Attempted to amend the current location feature, which is not currently working. I also can't make the Fahrenheit conversion work, I will look into this more now.

--- src/app.js
+++ src/app.js
@@ -1,69 +1,59 @@
-// // DATE & TIME
 
-// function formatDate() {
-//   let dayIndex = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday"
-//   ];
-//   let day = dayIndex[currentDate.getDay()];
-
-//   let hours = currentDate.getHours();
-//   if (hours < 10) {
-//     hours = `0${hours}`;
-//   }
-
-//   let minutes = currentDate.getMinutes();
-//   if (minutes < 10) {
-//     minutes = `0${minutes}`;
-//   }
-//   return `${day}, ${hours}:${minutes}`;
-// }
-//scattered clouds
-
-// Date & Time 
 
 function formatDate(timestamp) {
- let date = new Date(timestamp);
- let hours = date.getHours();
+let date = new Date(timestamp);
+let hours = date.getHours();
+let minutes = date.getMinutes();
+if (minutes < 10) {
+minutes = `0${minutes}`;
+ }
+
   if (hours < 10) {
   hours = `0${hours}`;
   }
- let minutes = date.getMinutes();
-  if (minutes < 10) {
-  minutes = `0${minutes}`; 
-  }
 
-let days = [ "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-let day = days[date.getDay()];
-return `${day} ${hours}:${minutes}`;
+ let days = [ "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+ let day = days[date.getDay()];
+ return `${day}, ${hours}:${minutes}`;
+
 }
+
+function formatHours(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+let minutes = date.getMinutes();
+if (minutes < 10) {
+minutes = `0${minutes}`;
+}
+if (hours < 10) {
+hours = `0${hours}`;
+}
+
+ return `${hours}:${minutes}`;
+}
+
+
 
 function displayTemperature(response) {
-let temperatureElement = document.querySelector("#temperature");
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let windElement = document.querySelector("#wind");
-let dateElement = document.querySelector("#date");
-let iconElement = document.querySelector("#icon");
-
+document.querySelector.innerHTML = response.data.name;
 celsiusTemperature = response.data.main.temp;
-
-temperatureElement.innerHTML= Math.round(response.data.main.temp);
+let cityElement = document.querySelector("#city");
 cityElement.innerHTML = response.data.name;
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML= Math.round(response.data.main.temp);
+let descriptionElement = document.querySelector("#description");
 descriptionElement.innerHTML = response.data.weather[0].description;
+let humidityElement = document.querySelector("#humidity");
 humidityElement.innerHTML = response.data.main.humidity;
+let windElement = document.querySelector("#wind");
 windElement.innerHTML = Math.round(response.data.wind.speed);
+let dateElement = document.querySelector("#date");
 dateElement.innerHTML = formatDate(response.data.dt * 1000)
+let iconElement = document.querySelector("#icon");
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
 iconElement.setAttribute("alt", response.data.weather[0].description);
-}
 
+}
 
 function search(city) {
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -74,7 +64,7 @@ axios.get(apiUrl).then(displayTemperature);
 function handleSubmit(event) {
  event.preventDefault();
  let cityInputElement = document.querySelector("#city-input");
- search(CityInputElement.value);
+ search(cityInputElement.value);
 }
 
 let celciusTemperature = null;
@@ -102,27 +92,11 @@ function startGeolocation(event) {
 let position = document.querySelector("#current-city-btn");
 position.addEventListener("click", startGeolocation);
 
-// search("Maidstone")
-
-// function handleSubmit(event) {
-// event.preventDefault();
-// // let cityInputElement = document.querySelector("#city-input");
-// }
-
-// search("Maidstone");
-
-
-// let currentDate = new Date();
-// let date = document.querySelector("#date");
-// date.innerHTML = formatDate(currentDate);
-
-// Unit Conversion
-
 function displayFahrenheitTemperature(event) {
 event.preventDefault();
-celsiusLink.classList.remove("active");
-fahrenheitLink.classlist.add("active");
 let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+fahrenheitLink.classlist.add("active");
+celsiusLink.classList.remove("active");
 temperatureElement.innerHTML = Math.round(fahrenheitTemperature)
  }
 
@@ -136,87 +110,10 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null 
 
-let form = document.querySelector("#search-form");
-
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFarenheitTemperature);
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Maidstone");
-
-//   function showTemperature(response) {
-//   celsiusTemperature = response.data.main.temp;
-//   let cityName = document.querySelector("#city");
-//   cityName.innerHTML = response.data.name;
-//   let temperatureElement = document.querySelector("#temperature");
-//   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-// }
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   let citySearch = document.querySelector("#city-input");
-//   let cityName = document.querySelector("#city");
-//   cityName.innerHTML = citySearch.value;
-//   let descriptionElement = document.querySelector("#description");
-//   let humidityElement = document.querySelector("#humidity");
-//   let windElement = document.querySelector("#wind");
-//   let dateElement = document.querySelector("#date");
-//   let iconElement = document.querySelector("#icon");
-//   searchCity(citySearch.value);
-// }
-
-// function searchCity(city) {
-//   apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Maidstone&&appid=${apiKey}`;
-//   }
-// let searchForm = document.querySelector("#search-form");
-// searchForm.addEventListener("submit", handleSubmit);
-
-// // CURRENT-CITY BUTTON
-
-// function getPosition(position) {
-//   let lat = position.coords.latitude;
-//   let lon = position.coords.longitude;
-//   let unit = "metric"; // metric for celsius
-//   apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`
-//   axios.get(apiUrl).then(showTemperature);
-// }
-
-
-// function displayFahrenheit(event){
-// event.preventDefault();
-// let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
-// fahrenheitLink.classList.add("active");
-// celsiusLink.classList.remove("active");
-// let temperatureElement = document.querySelector("#temperature");
-// temperatureElement.innerHTML = Math.round(fahrenheitTemperature)
-// }
-
-// temperatureElement.innerHTML = Math.round(response.data.main.temp);
-// cityElement.innerHTML = response.data.name;
-// descriptionElement.innerHTML = response.data.weather[0].description;
-// humidityElement.innerHTML = response.data.main.humidity;
-// windElement.innerHTML = Math.round(response.data.wind.speed);
-// dateElement.innerHTML = Math.round(response.data.dt *1000);
-// iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-//  iconElement.setAttribute("alt", response.data.weather[0].description);
-  
-// function displayCelsius(event){
-// event.preventDefault();
-// fahrenheitLink.classList.remove("active");
-// celsiusLink.classList.add("active");
-// let temperatureElement = document.querySelector("#temperature");
-// temperatureElement.innerHTML = Math.round(celsiusTemperature)
-// }
-
-// let celsiusLink = document.querySelector("#celsius-link");
-// celsiusLink.addEventListener("click", displayCelsius);
-
-// function startGeolocation(event) {
-//   event.preventDefault();
-//   navigator.geolocation.getCurrentPosition(getPosition);
-// }
-
-// let position = document.querySelector("#current-city-btn");
-// position.addEventListener("click", startGeolocation);
